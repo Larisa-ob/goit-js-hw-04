fe7815cd439fbe26038341242cf110e8dfff4afa
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-10-05 21:58:48 +0300

    task-4-2 без function

---
+++ js/task-4-2.js
@@ -0,0 +1,35 @@
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  showItems() {
+    return this.items;
+  },
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
+console.table(inventory.showItems());
+inventory.add("Medkit");
+console.table(inventory.showItems());
+
+const invokeInventoryAction = function (itemName, action {
+  console.log(`Invoking action on ${itemName}`);
+  action;
+};
+
+invokeInventoryAction("Medkit", inventory.add("Medkit"));
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+/*invokeInventoryAction("Gas mask", inventory.remove);*/
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
